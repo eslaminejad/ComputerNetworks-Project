ced788dbd062e8383e76289348c4d08a89ab10ba
Ali <aeslami111@gmail.com>
2022-08-04 22:47:01 +0430

    copy code from mohammad and change threads + no need to login for streaming

--- client.py
+++ client.py
@@ -1,12 +1,123 @@
 import socket, threading
 
+
+import cv2, imutils, socket
+import numpy as np
+import time, os
+import base64
+import threading, wave, pyaudio,pickle,struct
+
+BUFF_SIZE = 65536
+
+stream_port = 9688
+
 host = '127.0.0.1'
 port = 8550
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+file_port = 8551
+
 # connect to webserver
 client.connect((host, port))
 print("connected to server")
+
+
+def video_stream(stream_socket):
+    cv2.namedWindow('RECEIVING VIDEO')
+    cv2.moveWindow('RECEIVING VIDEO', 10, 360)
+    fps, st, frames_to_count, cnt = (0, 0, 20, 0)
+    while True:
+        packet, _ = stream_socket.recvfrom(BUFF_SIZE)
+        data = base64.b64decode(packet, ' /')
+        npdata = np.fromstring(data, dtype=np.uint8)
+
+        frame = cv2.imdecode(npdata, 1)
+        frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow("RECEIVING VIDEO", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('q'):
+            stream_socket.close()
+            os._exit(1)
+            break
+
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count / (time.time() - st))
+                st = time.time()
+                cnt = 0
+            except:
+                pass
+        cnt += 1
+
+    stream_socket.close()
+    cv2.destroyAllWindows()
+
+
+def audio_stream():
+    p = pyaudio.PyAudio()
+    CHUNK = 1024
+    print("line 60")
+    stream = p.open(format=p.get_format_from_width(2),
+                    channels=2,
+                    rate=44100,
+                    output=True,
+                    frames_per_buffer=CHUNK)
+
+    # create socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_address = (host, stream_port - 1)
+    print('server listening at', socket_address)
+    client_socket.connect(socket_address)
+    print("CLIENT CONNECTED TO", socket_address)
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        try:
+            while len(data) < payload_size:
+                packet = client_socket.recv(4 * 1024)  # 4K
+                if not packet: break
+                data += packet
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack("Q", packed_msg_size)[0]
+            while len(data) < msg_size:
+                data += client_socket.recv(4 * 1024)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data)
+            stream.write(frame)
+
+        except:
+            break
+
+    client_socket.close()
+    os._exit(1)
+
+
+def get_stream():
+    print("line 99")
+    stream_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    stream_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
+
+    message = b'Hello'
+    stream_socket.sendto(message, (host, stream_port))
+    print("line 105")
+
+    thread1 = threading.Thread(target=audio_stream, args=([]))
+    thread1.start()
+
+
+    thread2 = threading.Thread(target=video_stream, args=([stream_socket]))
+    thread2.start()
+    thread2.join()
+    thread1.join()
+    # from concurrent.futures import ThreadPoolExecutor
+    # with ThreadPoolExecutor(max_workers=2) as executor2:
+    #     print("line 107")
+    #     executor2.submit(audio_stream)
+    #     executor2.submit(video_stream, stream_socket)
+
 
 def echo():
     while True:
@@ -16,14 +127,21 @@ def echo():
             command = message.split()[0]
             if command == 'upload':
                 filename = message.split()[2]
-                with open(filename, 'rb') as file:
-                    sendfile = file.read()
                 client.send(('upload ' + message.split()[1]).encode('ascii'))
-                client.sendall(sendfile)
+                file_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                file_client.connect((host, file_port))
+                with file_client, open(filename, 'rb') as file:
+                    sendfile = file.read()
+                    file_client.sendall(sendfile)
                 print('file sent')
+            elif command == 'stream':
+                client.send(message.encode('ascii'))
+                # TODO
+                get_stream()
             else:
                 client.send(message.encode('ascii'))
         except IOError as e:
+            print(e)
             print("io error")
         except socket.error:
             print("socket error")
--- server.py
+++ server.py
@@ -1,21 +1,41 @@
-import random
-import socket, threading
+import socket
+import threading
+
+import cv2, imutils, socket
+import numpy as np
+import time
+import base64
+import threading, wave, pyaudio,pickle,struct
+import sys
+import queue
+import os
+
+BUFF_SIZE = 65536
+
 
 host = '127.0.0.1'
 port = 8550
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen(1)
+file_port = 8551
+file_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+file_server.bind((host, file_port))
+file_server.listen(2)
 print("server listening")
 
-users = {'ali':'123'}
+users = {'ali': '123'}
 
-class User():
+stream_port = 9688
+stream_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+stream_socket.bind((host, stream_port))
+
+class User:
     username = ''
     password = ''
     logged_in = False
 
-    def register(self,inputusername, inputpassword):
+    def register(self, inputusername, inputpassword):
         if inputusername not in users:
             self.username = inputusername
             self.password = inputpassword
@@ -25,7 +45,7 @@ class User():
         else:
             return False
 
-    def login(self,inputusername, inputpassword):
+    def login(self, inputusername, inputpassword):
         if inputusername in users:
             if users[inputusername] == inputpassword:
                 self.username = inputusername
@@ -42,12 +62,13 @@ class User():
         else:
             return False
 
+
 class Video():
-    owner : str
-    likes : list
-    dislikes : list
-    comments : list
-    title : str
+    owner: str
+    likes: list
+    dislikes: list
+    comments: list
+    title: str
 
     def __init__(self, owner, title):
         self.owner = owner
@@ -56,21 +77,25 @@ class Video():
         self.dislikes = []
         self.comments = []
 
-    def upload(self, soc):
-        savefilename = 'data/'+self.title+'.mp4'
+    def upload(self):
+        soc, addr = file_server.accept()
+        print(f'client connect to upload file {addr}')
+        savefilename = 'data/' + self.title
         with soc, open(savefilename, 'wb') as file:
+            recvfile = soc.recv(4096)
             while True:
-                recvfile = soc.recv(4096)
-                print(recvfile)
-                if not recvfile: break
                 file.write(recvfile)
+                recvfile = soc.recv(4096)
+                if not recvfile:
+                    break
         print("File has been received.")
 
-def handle_user(message,user):
+
+def handle_user(message, user):
     if message.split()[0] == 'register' and len(message.split()) == 3:
         inputusername = message.split()[1]
         inputpassword = message.split()[2]
-        register = user.register(inputusername,inputpassword)
+        register = user.register(inputusername, inputpassword)
         if register == True:
             return 'register successful'
         else:
@@ -79,7 +104,7 @@ def handle_user(message,user):
     elif message.split()[0] == 'login' and len(message.split()) == 3:
         inputusername = message.split()[1]
         inputpassword = message.split()[2]
-        login = user.login(inputusername,inputpassword)
+        login = user.login(inputusername, inputpassword)
         if login == True:
             return 'login successful'
         else:
@@ -99,17 +124,22 @@ def handle(client: socket.socket):
         try:
             message = client.recv(1024).decode('ascii')
             print(message)
-            if message.split()[0] in ['login','logout','register']:
-                result = handle_user(message,user, )
+            if message.split()[0] in ['login', 'logout', 'register']:
+                result = handle_user(message, user, )
                 client.send(result.encode('ascii'))
 
             elif message.split()[0] == 'upload':
-                if user.logged_in :
+                if user.logged_in:
                     title = message.split()[1]
                     video = Video(user.username, title)
-                    video.upload(client)
+                    video.upload()
                 else:
                     client.send('you need to login'.encode('ascii'))
+            elif message.split()[0] == 'stream':
+                client.send('be prepare'.encode('ascii'))
+                filename = 'data/' + message.split()[1]
+                stream_video(filename)
+
 
         except Exception as e:
             print(e)
@@ -118,6 +148,125 @@ def handle(client: socket.socket):
             break
 
 
+def video_stream_gen(vid, q):
+    WIDTH = 400
+    while (vid.isOpened()):
+        try:
+            _, frame = vid.read()
+            frame = imutils.resize(frame, width=WIDTH)
+            q.put(frame)
+        except:
+            os._exit(1)
+    print('Player closed')
+    BREAK = True
+    vid.release()
+
+
+def video_stream(q, FPS):
+    global TS
+    fps, st, frames_to_count, cnt = (0, 0, 1, 0)
+    cv2.namedWindow('TRANSMITTING VIDEO')
+    cv2.moveWindow('TRANSMITTING VIDEO', 10, 30)
+    while True:
+        msg, client_addr = stream_socket.recvfrom(BUFF_SIZE)
+        print('GOT connection from ', client_addr)
+        WIDTH = 400
+
+        while (True):
+            frame = q.get()
+            encoded, buffer = cv2.imencode('.jpeg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+            message = base64.b64encode(buffer)
+            stream_socket.sendto(message, client_addr)
+            frame = cv2.putText(frame, 'FPS: ' + str(round(fps, 1)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                                (0, 0, 255), 2)
+            if cnt == frames_to_count:
+                try:
+                    fps = (frames_to_count / (time.time() - st))
+                    st = time.time()
+                    cnt = 0
+                    if fps > FPS:
+                        TS += 0.001
+                    elif fps < FPS:
+                        TS -= 0.001
+                    else:
+                        pass
+                except:
+                    pass
+            cnt += 1
+
+            cv2.imshow('TRANSMITTING VIDEO', frame)
+            key = cv2.waitKey(int(1000 * TS)) & 0xFF
+            if key == ord('q'):
+                os._exit(1)
+                TS = False
+                break
+
+
+def audio_stream(filename):
+    s = socket.socket()
+    s.bind((host, (stream_port - 1)))
+
+    s.listen(5)
+    CHUNK = 1024
+    print("line 211")
+    try:
+        os.remove('data/temp.wav')
+    except:
+        pass
+    command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(filename, 'data/temp.wav')
+    os.system(command)
+    print("line 219")
+    wf = wave.open("data/temp.wav", 'rb')
+    p = pyaudio.PyAudio()
+    print('server listening at', (host, (stream_port - 1)))
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    client_socket, addr = s.accept()
+
+    while True:
+        if client_socket:
+            while True:
+                data = wf.readframes(CHUNK)
+                a = pickle.dumps(data)
+                message = struct.pack("Q", len(a)) + a
+                client_socket.sendall(message)
+
+
+def stream_video(filename):
+    q = queue.Queue(maxsize=10)
+    vid = cv2.VideoCapture(filename)
+    FPS = vid.get(cv2.CAP_PROP_FPS)
+    global TS
+    TS = (0.5 / FPS)
+    BREAK = False
+    print('FPS:', FPS, TS)
+    totalNoFrames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    durationInSeconds = float(totalNoFrames) / float(FPS)
+    d = vid.get(cv2.CAP_PROP_POS_MSEC)
+    print(durationInSeconds, d)
+
+    thread1 = threading.Thread(target=audio_stream, args=([filename]))
+    thread1.start()
+
+    thread2 = threading.Thread(target=video_stream_gen, args=([vid, q]))
+    thread2.start()
+
+    thread3 = threading.Thread(target=video_stream, args=([q, FPS]))
+    thread3.start()
+
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    # from concurrent.futures import ThreadPoolExecutor
+    # with ThreadPoolExecutor(max_workers=3) as executor:
+    #     executor.submit(audio_stream, filename)
+    #     executor.submit(video_stream_gen, vid, q)
+    #     executor.submit(video_stream, q, FPS)
 
 while True:
     client, address = server.accept()
@@ -128,3 +277,4 @@ while True:
 
     thread = threading.Thread(target=handle, args=([client]))
     thread.start()
+    thread.join()
